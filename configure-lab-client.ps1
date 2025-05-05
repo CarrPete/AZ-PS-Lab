c296ece86f87593bed718aed32ba2cea38ac1c36
CarrPete <carrpete@hotmail.com>
2025-05-05 16:15:30 -0600

    Update configure-lab-client.ps1

--- configure-lab-client.ps1
+++ configure-lab-client.ps1
@@ -70,9 +70,9 @@ catch {
 }
 
 # Configure DNS to point to DC
-Write-Log "Configuring DNS to 10.0.1.4"
+Write-Log "Configuring DNS to 10.0.1.10"
 try {
-    Set-DnsClientServerAddress -InterfaceAlias $InterfaceAlias -ServerAddresses ("10.0.1.4") -ErrorAction Stop
+    Set-DnsClientServerAddress -InterfaceAlias $InterfaceAlias -ServerAddresses ("10.0.1.10") -ErrorAction Stop
     Write-Log "DNS configured successfully"
 }
 catch {
